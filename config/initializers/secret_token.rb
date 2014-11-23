0029c0003cd213bf85b30e9e52f73d8e004ab7fe
FedericoSoma <fredrikkk@hotmail.it>
2014-11-23 10:45:35 +0100

    prima modifica

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tree::Application.config.secret_token = 'e24305be86712d1d5b2a93aa09027a510e09ee93648c9aaeedd13931e57f229e9ff8f16bd02e9022689bb287a32703cef435596b93fd4f234f3a4553da9b2c4a'
